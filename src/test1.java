3ddbb44b0adb75f380cbbd71a4548ad7ee451fb2
AkdenizO <akdenizozal@gmail.com>
2020-11-24 19:34:27 +0100

    version 1

---
+++ src/test1.java
@@ -0,0 +1,8 @@
+public class test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        System.out.println(".gitignore dosyasina hangi dosya veya klasörler gitmeyacekse onlar yazilir ");
+    }
+}
